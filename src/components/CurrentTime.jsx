950cdb5ad2839dc9619d08381b36f5c48cf22d8e
dovhanrg <dovhan.rg@gmail.com>
2019-10-24 18:01:35 +0300

    made CurrentTime component to function

--- src/components/CurrentTime.jsx
+++ src/components/CurrentTime.jsx
@@ -1,41 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+const CurrentTime = props => {
+
+  const [date, setDate] = useState({
+    date: new Date().toLocaleTimeString()
+  });
+  useEffect(
+    () => {
+      setInterval(
+        () => {
+          setDate({
+            date: new Date().toLocaleTimeString()
+          })
+        }, 1000)
+    }, [])
 
 
-class CurrentTime extends React.Component {
-  constructor(props) {
-    super(props);
-    // console.log(props.)
-    this.state = {
-      date: new Date(),
-      sign: this.props.sign
-    }
-  }
-
-  componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(),
-      1000
-    );
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-
-  tick() {
-    this.setState({ date: new Date() })
-  }
-
-  render() {
-    return (
-      <div className="currentTime" >
-        <h4>
-          Now is {this.state.date.toLocaleTimeString()}
-        </h4>
-        <p><small>{ this.state.sign }</small></p>
-      </div>
-    )
-  }
-}
+  return (
+    <div className="currentTime" >
+      <h4>
+        Now is {date.date}
+      </h4>
+      <p><small>{props.sign}</small></p>
+    </div>
+  )
+};
 
 export default CurrentTime
